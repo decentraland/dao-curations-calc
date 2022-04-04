985506ab82ad5d89bf4bedd1e54c5fb01aad3bc0
Yemel Jardi <angel.jardi@gmail.com>
2022-04-04 14:34:54 -0300

    process payment addresses

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { ethers } from 'ethers';
 import fetch from 'isomorphic-fetch'
 import { createObjectCsvWriter } from 'csv-writer'
 
@@ -12,7 +13,7 @@ async function main() {
         headers: {
           'content-type': 'application/json'
         },
-        body: `{\"query\":\"{\\n  curations(orderBy: timestamp, orderDirection: asc, first: 1000, where: { timestamp_gte: ${timestamp} }, block: { number: 25696595 }) {\\n    curator {\\n      id\\n    }\\n    collection {\\n      id\\n      itemsCount\\n      name\\n    }\\n    isApproved\\n    timestamp\\n    txHash\\n  }\\n}\\n\",\"variables\":null}`,
+        body: `{\"query\":\"{\\n  curations(orderBy: timestamp, orderDirection: asc, first: 1000, where: { timestamp_gte: ${timestamp} }, block: { number: 26617168 }) {\\n    curator {\\n      id\\n    }\\n    collection {\\n      id\\n      itemsCount\\n      name\\n    }\\n    isApproved\\n    timestamp\\n    txHash\\n  }\\n}\\n\",\"variables\":null}`,
         method: 'POST'
       }
     )
@@ -81,7 +82,46 @@ async function main() {
     curators[collection[1].curator.id] += collection[1].collection.itemsCount
   }
 
-  console.log(curators)
+  console.log("Total Curations");
+  console.log(curators);
+
+  const lastTotal : { [curator: string]: number } = {
+    '0x5d7846007c1dd6dca25d16ce2f71ec13bcdcf6f0': 218,
+    '0x716954738e57686a08902d9dd586e813490fee23': 130,
+    '0x7a3891acf4f3b810992c4c6388f2e37357d7d3ab': 373,
+    '0x82d54417fc69681dc74a6c0c68c6dbad5a2857b9': 446,
+    '0x8938d1f65abe7750b0777206ee26c974a6721194': 599,
+    '0x91e222ed7598efbcfe7190481f2fd14897e168c8': 386,
+    '0x399a44f5821b1f859bc236e14367c4f7c36933fb': 140,
+    '0x967fb0c36e4f5288f30fb05f8b2a4d7b77eaca4b': 94,
+    '0x5e382071464a6f9ea29708a045983dc265b0d86d': 142,
+    '0xc8ad6322821b51da766a4b2a82b39fb72b53d276': 92,
+    '0xa8c7d5818a255a1856b31177e5c96e1d61c83991': 30,
+    '0x336685bb3a96e13b77e909d7c52e8afcff1e859e': 94,
+    '0x41eb5f82af60873b3c14fedb898a1712f5c35366': 42,
+    '0x470c33abd57166940095d59bd8dd573cbae556c3': 49,
+    '0x1dec5f50cb1467f505bb3ddfd408805114406b10': 14,
+    '0x805797df0c0d7d70e14230b72e30171d730da55e': 265,
+  };
+  const newTotal : { [curator: string]: number } = {};
+  const newPayment : { [curator: string]: number } = {};
+  const curatorsAddresses = Object.keys(curators);
+  for(var i = 0; i < curatorsAddresses.length; i++) {
+    let address = String(curatorsAddresses[i]);
+    newTotal[address] = curators[address] - lastTotal[address];
+
+    if (newTotal[address] > 0) {
+      let paddress = ethers.utils.getAddress(paymentAddress[address]);
+      newPayment[paddress] = newTotal[address] * 10;
+    }
+  }
+
+  console.log('New Total');
+  console.log(newTotal);
+
+  console.log('New Payment');
+  console.log(newPayment);
+
 }
 
 void main()
@@ -104,6 +144,25 @@ const names: { [curator: string]: string } = {
   '0x470c33abd57166940095d59bd8dd573cbae556c3': 'James Guard ',
   '0x1dec5f50cb1467f505bb3ddfd408805114406b10': 'Fabeeo',
   '0x805797df0c0d7d70e14230b72e30171d730da55e': 'Yannakis',
+}
+
+const paymentAddress: { [curator: string]: string } = {
+  '0x5d7846007c1dd6dca25d16ce2f71ec13bcdcf6f0': '0x5d7846007c1dd6dca25d16ce2f71ec13bcdcf6f0',
+  '0x716954738e57686a08902d9dd586e813490fee23': '0x716954738e57686a08902d9dd586e813490fee23',
+  '0x7a3891acf4f3b810992c4c6388f2e37357d7d3ab': '0xC958f028d1b871ab2e32C2aBdA54F37191eFe0C2',
+  '0x82d54417fc69681dc74a6c0c68c6dbad5a2857b9': '0x82d54417fc69681dc74a6c0c68c6dbad5a2857b9',
+  '0x8938d1f65abe7750b0777206ee26c974a6721194': '0x9dB59920d3776c2d8A3aA0CbD7b16d81FcAb0A2b',
+  '0x91e222ed7598efbcfe7190481f2fd14897e168c8': '0x91e222ed7598efbcfe7190481f2fd14897e168c8',
+  '0x399a44f5821b1f859bc236e14367c4f7c36933fb': '0x9dB59920d3776c2d8A3aA0CbD7b16d81FcAb0A2b',
+  '0x967fb0c36e4f5288f30fb05f8b2a4d7b77eaca4b': '0x6cDFDB9a4D99f16B5607caB1d00c792206db554E',
+  '0x5e382071464a6f9ea29708a045983dc265b0d86d': '0x862f109696d7121438642a78B3CaA38F476db08b',
+  '0xc8ad6322821b51da766a4b2a82b39fb72b53d276': '0xc8ad6322821b51da766a4b2a82b39fb72b53d276',
+  '0xa8c7d5818a255a1856b31177e5c96e1d61c83991': '0xa8c7d5818A255A1856b31177E5c96E1D61c83991',
+  '0x336685bb3a96e13b77e909d7c52e8afcff1e859e': '0x336685bb3A96E13B77E909d7C52e8AfCfF1E859E',
+  '0x41eb5f82af60873b3c14fedb898a1712f5c35366': '0x41eb5F82af60873b3C14fEDB898A1712f5c35366',
+  '0x470c33abd57166940095d59bd8dd573cbae556c3': '0x470c33aBD57166940095d59BD8Dd573cBae556c3',
+  '0x1dec5f50cb1467f505bb3ddfd408805114406b10': '0x1DeC5f50cB1467F505BB3ddFD408805114406b10',
+  '0x805797df0c0d7d70e14230b72e30171d730da55e': '0x805797Df0c0d7D70E14230b72E30171d730DA55e',
 }
 
 function exportCurations(collectionsList: [string, Curation][]) {
