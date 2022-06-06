94b90ff877baa56c40d848f408f47beef4ffcd3a
Yemel Jardi <angel.jardi@gmail.com>
2022-06-06 11:36:30 -0300

    new payment

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ async function main() {
         headers: {
           'content-type': 'application/json'
         },
-        body: `{\"query\":\"{\\n  curations(orderBy: timestamp, orderDirection: asc, first: 1000, where: { timestamp_gte: ${timestamp} }, block: { number: 27820000 }) {\\n    curator {\\n      id\\n    }\\n    collection {\\n      id\\n      itemsCount\\n      name\\n    }\\n    isApproved\\n    timestamp\\n    txHash\\n  }\\n}\\n\",\"variables\":null}`,
+        body: `{\"query\":\"{\\n  curations(orderBy: timestamp, orderDirection: asc, first: 1000, where: { timestamp_gte: ${timestamp} }, block: { number: 29050000 }) {\\n    curator {\\n      id\\n    }\\n    collection {\\n      id\\n      itemsCount\\n      name\\n    }\\n    isApproved\\n    timestamp\\n    txHash\\n  }\\n}\\n\",\"variables\":null}`,
         method: 'POST'
       }
     )
@@ -90,21 +90,23 @@ async function main() {
     '0x716954738e57686a08902d9dd586e813490fee23': 133,
     '0x7a3891acf4f3b810992c4c6388f2e37357d7d3ab': 373,
     '0x82d54417fc69681dc74a6c0c68c6dbad5a2857b9': 456,
-    '0x8938d1f65abe7750b0777206ee26c974a6721194': 660,
-    '0x91e222ed7598efbcfe7190481f2fd14897e168c8': 388,
+    '0x8938d1f65abe7750b0777206ee26c974a6721194': 685,
+    '0x91e222ed7598efbcfe7190481f2fd14897e168c8': 392,
     '0x399a44f5821b1f859bc236e14367c4f7c36933fb': 140,
     '0x967fb0c36e4f5288f30fb05f8b2a4d7b77eaca4b': 94,
-    '0x5e382071464a6f9ea29708a045983dc265b0d86d': 254,
-    '0xc8ad6322821b51da766a4b2a82b39fb72b53d276': 130,
-    '0xa8c7d5818a255a1856b31177e5c96e1d61c83991': 212,
-    '0x336685bb3a96e13b77e909d7c52e8afcff1e859e': 166,
-    '0x41eb5f82af60873b3c14fedb898a1712f5c35366': 123,
+    '0x5e382071464a6f9ea29708a045983dc265b0d86d': 289,
+    '0xc8ad6322821b51da766a4b2a82b39fb72b53d276': 133,
+    '0xa8c7d5818a255a1856b31177e5c96e1d61c83991': 258,
+    '0x336685bb3a96e13b77e909d7c52e8afcff1e859e': 220,
+    '0x41eb5f82af60873b3c14fedb898a1712f5c35366': 247,
     '0x470c33abd57166940095d59bd8dd573cbae556c3': 52,
-    '0x1dec5f50cb1467f505bb3ddfd408805114406b10': 47,
-    '0x805797df0c0d7d70e14230b72e30171d730da55e': 632
+    '0x1dec5f50cb1467f505bb3ddfd408805114406b10': 110,
+    '0x805797df0c0d7d70e14230b72e30171d730da55e': 841
   };
   const newTotal : { [curator: string]: number } = {};
   const newPayment : { [curator: string]: number } = {};
+  var newPamentCSV = 'token_type,token_address,receiver,amount\n';
+
   const curatorsAddresses = Object.keys(curators);
   for(var i = 0; i < curatorsAddresses.length; i++) {
     let address = String(curatorsAddresses[i]);
@@ -113,6 +115,7 @@ async function main() {
     if (newTotal[address] > 0) {
       let paddress = ethers.utils.getAddress(paymentAddress[address]);
       newPayment[paddress] = newTotal[address] * 10;
+      newPamentCSV += `erc20,0x0F5D2fB29fb7d3CFeE444a200298f468908cC942,${paddress},${newPayment[paddress]}\n`;
     }
   }
 
@@ -122,6 +125,8 @@ async function main() {
   console.log('New Payment');
   console.log(newPayment);
 
+  console.log('=== CSV For Gnosis Transfer ===');
+  console.log(newPamentCSV);
 }
 
 void main()
