c5c73b4207fd2fbaaebcadfd29336b6eb22dbff6
Yemel Jardi <angel.jardi@gmail.com>
2022-08-09 11:45:02 -0300

    aug payment

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { createObjectCsvWriter } from 'csv-writer'
 
 async function main() {
   const curationsList: Curation[] = []
+  const blocknumer = 31690000;
   let timestamp = '0';
 
   while (true) {
@@ -13,7 +14,7 @@ async function main() {
         headers: {
           'content-type': 'application/json'
         },
-        body: `{\"query\":\"{\\n  curations(orderBy: timestamp, orderDirection: asc, first: 1000, where: { timestamp_gte: ${timestamp} }, block: { number: 30640000 }) {\\n    curator {\\n      id\\n    }\\n    collection {\\n      id\\n      itemsCount\\n      name\\n    }\\n    isApproved\\n    timestamp\\n    txHash\\n  }\\n}\\n\",\"variables\":null}`,
+        body: `{\"query\":\"{\\n  curations(orderBy: timestamp, orderDirection: asc, first: 1000, where: { timestamp_gte: ${timestamp} }, block: { number: ${blocknumer} }) {\\n    curator {\\n      id\\n    }\\n    collection {\\n      id\\n      itemsCount\\n      name\\n    }\\n    isApproved\\n    timestamp\\n    txHash\\n  }\\n}\\n\",\"variables\":null}`,
         method: 'POST'
       }
     )
@@ -75,8 +76,10 @@ async function main() {
     '0x470c33abd57166940095d59bd8dd573cbae556c3': 0,
     //Fabeeo
     '0x1dec5f50cb1467f505bb3ddfd408805114406b10': 0,
-    //Yannakis
+    //Yannakis Old
     '0x805797df0c0d7d70e14230b72e30171d730da55e': 0,
+    // Yannakis
+    '0x5ce9fb617333b8c5a8f7787710f7c07002cb3516': 0,
   }
   for (const collection of collectionsList) {
     curators[collection[1].curator.id] += collection[1].collection.itemsCount
@@ -86,22 +89,23 @@ async function main() {
   console.log(curators);
 
   const lastTotal : { [curator: string]: number } = {
-    '0x5d7846007c1dd6dca25d16ce2f71ec13bcdcf6f0': 247,
+    '0x5d7846007c1dd6dca25d16ce2f71ec13bcdcf6f0': 251,
     '0x716954738e57686a08902d9dd586e813490fee23': 133,
     '0x7a3891acf4f3b810992c4c6388f2e37357d7d3ab': 374,
     '0x82d54417fc69681dc74a6c0c68c6dbad5a2857b9': 456,
-    '0x8938d1f65abe7750b0777206ee26c974a6721194': 688,
+    '0x8938d1f65abe7750b0777206ee26c974a6721194': 736,
     '0x91e222ed7598efbcfe7190481f2fd14897e168c8': 394,
     '0x399a44f5821b1f859bc236e14367c4f7c36933fb': 140,
     '0x967fb0c36e4f5288f30fb05f8b2a4d7b77eaca4b': 94,
-    '0x5e382071464a6f9ea29708a045983dc265b0d86d': 443,
-    '0xc8ad6322821b51da766a4b2a82b39fb72b53d276': 152,
-    '0xa8c7d5818a255a1856b31177e5c96e1d61c83991': 339,
-    '0x336685bb3a96e13b77e909d7c52e8afcff1e859e': 253,
-    '0x41eb5f82af60873b3c14fedb898a1712f5c35366': 329,
+    '0x5e382071464a6f9ea29708a045983dc265b0d86d': 491,
+    '0xc8ad6322821b51da766a4b2a82b39fb72b53d276': 154,
+    '0xa8c7d5818a255a1856b31177e5c96e1d61c83991': 386,
+    '0x336685bb3a96e13b77e909d7c52e8afcff1e859e': 307,
+    '0x41eb5f82af60873b3c14fedb898a1712f5c35366': 390,
     '0x470c33abd57166940095d59bd8dd573cbae556c3': 52,
-    '0x1dec5f50cb1467f505bb3ddfd408805114406b10': 177,
-    '0x805797df0c0d7d70e14230b72e30171d730da55e': 1033
+    '0x1dec5f50cb1467f505bb3ddfd408805114406b10': 242,
+    '0x805797df0c0d7d70e14230b72e30171d730da55e': 1213,
+    '0x5ce9fb617333b8c5a8f7787710f7c07002cb3516': 0,
   };
   const newTotal : { [curator: string]: number } = {};
   const newPayment : { [curator: string]: number } = {};
@@ -148,7 +152,8 @@ const names: { [curator: string]: string } = {
   '0x41eb5f82af60873b3c14fedb898a1712f5c35366': 'Kristian',
   '0x470c33abd57166940095d59bd8dd573cbae556c3': 'James Guard ',
   '0x1dec5f50cb1467f505bb3ddfd408805114406b10': 'Fabeeo',
-  '0x805797df0c0d7d70e14230b72e30171d730da55e': 'Yannakis',
+  '0x805797df0c0d7d70e14230b72e30171d730da55e': 'Yannakis Old',
+  '0x5ce9fb617333b8c5a8f7787710f7c07002cb3516': 'Yannakis',
 }
 
 const paymentAddress: { [curator: string]: string } = {
@@ -168,6 +173,7 @@ const paymentAddress: { [curator: string]: string } = {
   '0x470c33abd57166940095d59bd8dd573cbae556c3': '0x470c33aBD57166940095d59BD8Dd573cBae556c3',
   '0x1dec5f50cb1467f505bb3ddfd408805114406b10': '0x1DeC5f50cB1467F505BB3ddFD408805114406b10',
   '0x805797df0c0d7d70e14230b72e30171d730da55e': '0x805797Df0c0d7D70E14230b72E30171d730DA55e',
+  '0x5ce9fb617333b8c5a8f7787710f7c07002cb3516': '0x5ce9fb617333b8c5a8f7787710f7c07002cb3516',
 }
 
 function exportCurations(collectionsList: [string, Curation][]) {
